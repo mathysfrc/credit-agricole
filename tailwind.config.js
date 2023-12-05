8d1b885173c81d0013fd3c58092cef173e978cf9
Mathys FRANCO <mathysf41@gmail.com>
2023-12-05 14:43:56 +0100

    création du formulaire de login avec style

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/**/*.js",
+    "./templates/**/*.html.twig",
+  ],
+  theme: {
+    extend: {},
+  },
+  colors: {
+    'darkblue': '#111827',
+    'white': '#FFFFFF',
+    'black': '#020617',
+    'blue': '#3D4797',
+    'grey': '#64748B',
+  },
+  plugins: [],
+}
